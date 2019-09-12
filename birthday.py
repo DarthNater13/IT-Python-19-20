283b97285f4134eb6727d6d832200f953e20a552
DarthNater13 <nsvsherman@gmail.com>
2019-09-12 14:16:46 -0400

    Gets calculate_days_between_dates working

--- birthday.py
+++ birthday.py
@@ -8,12 +8,11 @@ banner("BIRTHDAY", "Nate Sherman")
 # 1. Find out birthday from user
 # 2. Calculate how many days apart that is from now
 # 3. Print the birthday info, Days to go, Days ago, or Happy BDAY!
-
 def main():
     birthday = get_birthday_from_user()
     now = datetime.date.today()
-    calulate_days_between_dates(birthday, now)
-    print_birthday_info()
+    num_days = calulate_days_between_dates(birthday, now)
+    print_birthday_info(num_days)
 
 def get_birthday_from_user():
     print("What is your birthday?")
@@ -25,9 +24,11 @@ def get_birthday_from_user():
     return birthday
 
 def calulate_days_between_dates(date1, date2):
-    pass
+    this_year = datetime.date(date2.year, date1.month, date1.day)
+    dt = this_year - date2
+    return dt.days
 
-def print_birthday_info():
-    pass
+def print_birthday_info(number_of_days):
+    print(number_of_days)
 
 main()
