a667c2ce6dd678e725afb604ab2f780ce254dd0a
DarthNater13 <nsvsherman@gmail.com>
2019-09-24 14:11:50 -0400

    Functional HTML tag counter

--- htmlcount_ui.py
+++ htmlcount_ui.py
@@ -1,5 +1,6 @@
 from banner import banner
 import os
+import time
 def get_full_path(name):
     filename = os.path.join(".", "testing", f"{name}.html")
     return filename
@@ -8,36 +9,35 @@ def load(name):
     data = []
     filename = get_full_path(name)
     print(f"Loading {filename}")
+    time.sleep(1)
     if os.path.exists(filename):
         with open(filename) as fin:
-            for entry in fin.readlines():
-                alltag_count = data.count("<")
-                endtag_count = data.count("</")
-                tag_calc(alltag_count, endtag_count, filename)
-        return data
+            for line in fin.readlines():
+                data.append(line.rstrip())     
         print("File sucessfully loaded.")
+        time.sleep(1)
     else:
         print("ERROR: File doesn't exist.")
-
-def tag_calc(all_tag, end_tag, filename):
-    finaltag_count = int(all_tag - end_tag)
-    print(f"The file {filename} has {finaltag_count} tags")
+    return data
+def tag_calc(line):
+    return line.count("<") - line.count("</")
    
 #------------------------------------------------------------
 
 def main():
-    filename = input("Please enter the name of an HTML file: ")
-    tag_data = load(filename)
+    
     while True:
+        filename = input("Please enter the name of an HTML file: ")
+        tag_data = load(filename)
+        finaltag_count = 0
+        for line in tag_data:
+            finaltag_count += tag_calc(line)
+        print(f"The file {filename} has {finaltag_count} tags")
         start_again = input("Would you like to count another html file (Y/N)?")
-        if start_again.upper() == "Y":
-            main()
-            break
-        elif start_again.upper() == "N":
+        if start_again.upper() == "N":
             print("Thank you for using the HTML tag counter. Goodbye!")
             break
-        else:
-            print("Sorry, I don't understand")
+
 
 banner("HTML TAG COUNTER", "Nate Sherman")
 print("Welcome to the HTML tag counter")
